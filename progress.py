99005030a0118fa8cbe82a3febc3f55c98bc62c2
Automated Script <run@localhost>
2022-11-13 11:19:29 -0600

    AP/20221108TX45878/detail.json AP/20221108TX45878/summary.json AP/20221108TX45883/detail.json AP/20221108TX45883/summary.json AP/20221108TX45893/detail.json AP/20221108TX45893/summary.json AP/20221108TX45894/detail.json AP/20221108TX45894/summary.json AP/20221108WI50048/detail.json AP/20221108WI50048/summary.json AP/progress.json progress.py updated-urls.txt

--- progress.py
+++ progress.py
@@ -1,15 +1,25 @@
 import json
 import os
+import os.path
 import subprocess
 
 js = json.load(open('AP/progress.json'))
 urls = open('updated-urls.txt', 'w')
 
-for key, val in js.items():
-	os.mkdir(f'AP/{key}')
+for key, race in js.items():
+	path = f'AP/{key}'
 
-	ts = open(f'AP/{key}/timestamp')
+	if not os.path.exists(path):
+		os.mkdir(path)
 
-	urls.write(f"https://interactives.ap.org/election-results/data-live/2022-11-08/results/races/{item['statePostal']}/{item['raceID']}/detail.json")
-	urls.write(f"https://interactives.ap.org/election-results/data-live/2022-11-08/results/races/{item['statePostal']}/{item['raceID']}/metadata.json")
-	urls.write(f"https://interactives.ap.org/election-results/data-live/2022-11-08/results/races/{item['statePostal']}/{item['raceID']}/summary.json")
+	tspath = f'AP/{key}/timestamp'
+	timestamp = None
+
+	if os.path.exists(tspath):
+		ts = open(tspath)
+		timestamp = ts.read()
+		ts.close()
+
+	urls.write(f"https://interactives.ap.org/election-results/data-live/2022-11-08/results/races/{race['statePostal']}/{race['raceID']}/detail.json")
+	urls.write(f"https://interactives.ap.org/election-results/data-live/2022-11-08/results/races/{race['statePostal']}/{race['raceID']}/metadata.json")
+	urls.write(f"https://interactives.ap.org/election-results/data-live/2022-11-08/results/races/{race['statePostal']}/{race['raceID']}/summary.json")
