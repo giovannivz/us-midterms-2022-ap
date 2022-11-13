8a7a735bc081387f75f532f79ddea18e04bc4324
Automated Script <run@localhost>
2022-11-13 12:30:33 -0600

    AP/20221108TX45883/detail.json AP/20221108TX45883/summary.json AP/20221108TX45884/detail.json AP/20221108TX45884/summary.json AP/20221108TX45893/detail.json AP/20221108TX45893/summary.json AP/20221108TX45894/detail.json AP/20221108TX45894/summary.json AP/progress.json fetch-ap.sh updated-urls.txt

--- fetch-ap.sh
+++ fetch-ap.sh
@@ -2,7 +2,7 @@
 
 . ./funcs.sh
 
-download "AP" https://2022-election-production.newyorker.com/api/articles
+#download "AP" https://2022-election-production.newyorker.com/api/articles
 download "AP" https://interactives.ap.org/election-results/data-live/2022-11-08/results/national/progress.json
 download "AP" https://interactives.ap.org/election-results/data-live/2022-11-08/results/national/metadata.json
 
