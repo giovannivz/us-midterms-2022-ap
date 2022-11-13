84e6056d49ad927fc200abeace3d5dc804fa97c4
Automated Script <run@localhost>
2022-11-13 12:36:35 -0600

    AP/20221108TX45871/detail.json AP/20221108TX45871/summary.json AP/20221108TX45883/detail.json AP/20221108TX45883/summary.json AP/20221108TX45884/detail.json AP/20221108TX45884/summary.json AP/20221108TX45893/detail.json AP/20221108TX45893/summary.json AP/20221108TX45900/detail.json AP/20221108TX45900/summary.json AP/progress.json fetch-ap.sh updated-urls.txt

--- fetch-ap.sh
+++ fetch-ap.sh
@@ -3,8 +3,10 @@
 . ./funcs.sh
 
 #download "AP" https://2022-election-production.newyorker.com/api/articles
-download "AP" https://interactives.ap.org/election-results/data-live/2022-11-08/results/national/progress.json
-download "AP" https://interactives.ap.org/election-results/data-live/2022-11-08/results/national/metadata.json
+#download "AP" https://interactives.ap.org/election-results/data-live/2022-11-08/results/national/progress.json
+#download "AP" https://interactives.ap.org/election-results/data-live/2022-11-08/results/national/metadata.json
+
+python3 fetch-updated.py ap-base-urls.py
 
 python3 updated.py
 
