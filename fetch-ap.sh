d9093b9b948d4c2cfdf58658608bcfb608d7e885
Automated Script <run@localhost>
2022-11-13 12:30:35 -0600

    fetch-ap.sh updated-urls.txt

--- fetch-ap.sh
+++ fetch-ap.sh
@@ -8,6 +8,6 @@ download "AP" https://interactives.ap.org/election-results/data-live/2022-11-08/
 
 python3 updated.py
 
-cat updated-urls.txt | xargs -n 2 -P 5 ./download.sh
+cat updated-urls.txt | xargs -n 2 -P 1 ./download.sh
 
 #sleep 5
