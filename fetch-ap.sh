b48d29ab5fea0e99ff42be7e1ee3ec290e5db914
Automated Script <run@localhost>
2022-11-13 12:36:40 -0600

    AP/20221108TX45871/detail.json AP/20221108TX45875/detail.json AP/20221108TX45875/summary.json AP/progress.json fetch-ap.sh timestamps.json updated-urls.txt

--- fetch-ap.sh
+++ fetch-ap.sh
@@ -6,7 +6,7 @@
 #download "AP" https://interactives.ap.org/election-results/data-live/2022-11-08/results/national/progress.json
 #download "AP" https://interactives.ap.org/election-results/data-live/2022-11-08/results/national/metadata.json
 
-python3 fetch-updated.py ap-base-urls.py
+python3 fetch-updated.py ap-base-urls.txt
 
 python3 updated.py
 
