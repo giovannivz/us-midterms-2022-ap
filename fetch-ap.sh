d157caeaf400b9091662e350d003fb5c6c6b3186
Automated Script <run@localhost>
2022-11-13 11:29:46 -0600

    AP/20221108AZ4231/metadata.json AP/20221108CO6619/detail.json AP/20221108CO6619/summary.json AP/20221108CO7191/summary.json AP/20221108MD22916/detail.json AP/20221108MD22916/summary.json AP/20221108MI23816/detail.json AP/20221108MI23816/summary.json AP/20221108MI24074/detail.json AP/20221108MI24074/summary.json AP/20221108PA39017/detail.json AP/20221108PA39017/summary.json AP/20221108TX45871/detail.json AP/20221108TX45871/summary.json AP/20221108TX45878/detail.json AP/20221108TX45878/summary.json AP/20221108TX45883/detail.json AP/20221108TX45883/summary.json AP/20221108TX45893/detail.json AP/20221108TX45893/summary.json AP/20221108TX45900/detail.json AP/20221108TX45900/summary.json AP/20221108TX49477/detail.json AP/20221108TX49477/summary.json AP/progress.json fetch-ap.sh

--- fetch-ap.sh
+++ fetch-ap.sh
@@ -6,6 +6,4 @@ download "AP" https://2022-election-production.newyorker.com/api/articles
 download "AP" https://interactives.ap.org/election-results/data-live/2022-11-08/results/national/progress.json
 download "AP" https://interactives.ap.org/election-results/data-live/2022-11-08/results/national/metadata.json
 
-python3 updated.py
-
-cat updated-urls.txt | xargs -n 2 -P 5 ./download.sh
+cat ap-urls.txt | xargs -n 2 -P 5 ./download.sh
